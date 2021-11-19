31457b7dc39ca22cba76fbdef4185b7e550a0272
SaifL <leach_saif@yahoo.com>
2021-11-19 14:44:07 -0500

    made a functional sticky navbar

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-$("#sign-btn").click(function(){
-    var username = $("#username").val();
-    var password = $("#password").val();
-    $("#username").val("");
-    $("#password").val("");
-    console.log(username);
-    console.log(password);
-    $("#nav-name").text(username)
-})
+ $("#sign-btn").click(function(){
+     var username = $("#username").val();
+     var password = $("#password").val();
+     $("#username").val("");
+     $("#password").val("");
+     console.log(username);
+     console.log(password);
+     $("#nav-name").text(username)
+ })
 
-$("#log-btn").click(function(){
-    var username = $("#username").val();
-    var password = $("#password").val();
-    $("#username").val("");
-    $("#password").val("");
-    console.log(username);
-    console.log(password);
-    $("#nav-name").text(username)
-})
+ $("#log-btn").click(function(){
+     var username = $("#username").val();
+     var password = $("#password").val();
+     $("#username").val("");
+     $("#password").val("");
+     console.log(username);
+     console.log(password);
+     $("#nav-name").text(username)
+ })
+
+window.addEventListener("scroll" , function() {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky" , window.scrollY > 0);
+  
+  });
 
 // $("#nav-name").text(username)
